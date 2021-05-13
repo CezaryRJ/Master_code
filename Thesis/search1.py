e9e92d33981b2381a358876921413b6fde720ef2
Cezary Jaskula <cezy17@gmail.com>
2021-05-13 18:02:08 +0200

    small change on page 10

--- Thesis/search1.py
+++
@@ -1,9 +0,0 @@
-param = {
-"debugQuery":"off",
-"rows" : 10,
-"facet":"on", #if this is set to "off"
-"facet.field":"Date", #then no facet will be returned
-"facet.limit":-1, #no facet limit
-"facet.sort":"count", #sort by count
-"facet.mincount":0
-}
--- Thesis/search2.py
+++
@@ -1,8 +0,0 @@
-	param = {
-	"rows" : 0,
-	"facet":"on",
-	"facet.field":"Mailing-list",
-	"facet.limit":-1,
-	"facet.sort":"count",
-	"facet.mincount":0
-	}
